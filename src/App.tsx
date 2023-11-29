19efd82ef3fbf2d5b0ff50f65f9ac978fea9ac24
larijanim <larijani.maryam@yahoo.com>
2023-11-29 09:01:26 -0800

    add small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,12 @@
 
 import './App.css'
 import DataForm from "./DataForm";
-import UseFetchFollowers from "./UseFetchFollowers";
+// import UseFetchFollowers from "./UseFetchFollowers";
 
 import  { useEffect, useState } from "react";
 import SingleUser from "./SingleUser";
 import Followers from "./Followers";
 
-/*
-This coding test has five parts, with a 35-minute time limit. 
-You can earn bonus points by completing parts 4 and 5. 
-Please refer to the attached "task.jpg" for the desired output.
-Part 1:
-Create a component to input a GitHub username and fetch their profile data from "https://api.github.com/users/${qry}."
-Part 2:
-Display the user's profile card with login, avatar URL, creation date, and location. Add a "Find my Followers" button.
-Part 3:
-Clicking "Find my Followers" calls "https://api.github.com/users/${id}/followers" and displays their profiles as a grid.
-Part 4:
-Clicking "Find my Follower" in the grid updates the main profile card and the follower list.
-Part 5:
-Improve the UI using CSS.
-*/
 
 export default function App() {
   const [userProfile, setUserProfile] = useState("");
